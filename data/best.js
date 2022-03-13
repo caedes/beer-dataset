de54a735e127f0b363d79eda173f3dd5ff396fda
Romain Laurent <romain.laurent@cdiscount.com>
2022-03-14 00:31:14 +0100

    add random price on beers

--- data/best.js
+++ data/best.js
@@ -1,16 +1,20 @@
 const { prop, sortBy, uniqBy } = require("ramda");
 
+const sortById = sortBy(prop("id"));
+const uniqById = uniqBy(prop("id"));
+const randomInterval = (min, max) => Math.random() * (max - min) + min;
+
 const beers = require("../beer-database/best.json");
 
-const categories = sortBy(
-  prop("id"),
-  uniqBy(
-    prop("id"),
-    beers.map((beer) => beer.style.category)
-  )
-);
+const beersWithPrices = beers.map((beer) => ({
+  ...beer,
+  price: randomInterval(2, 8).toFixed(2),
+}));
+
+const categories = beers.map((beer) => beer.style.category);
+const sortedUniqCategories = sortById(uniqById(categories));
 
 module.exports = () => ({
-  beers,
-  categories,
+  beers: beersWithPrices,
+  categories: sortedUniqCategories,
 });
