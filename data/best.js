8116a290e08712e843986fd54e467b8c96c0697f
Romain Laurent <romain.laurent@cdiscount.com>
2022-03-15 12:29:51 +0100

    feat: make API length under 10k

--- data/best.js
+++ data/best.js
@@ -7,11 +7,18 @@ const randomInterval = (min, max) => Math.random() * (max - min) + min;
 const beers = require("../beer-database/best.json");
 
 const beersWithPrices = beers.map((beer) => ({
-  ...beer,
+  id: beer.id,
+  name: beer.name,
+  description: String(beer.description).substr(0, 120),
+  categoryId: beer.style.categoryId,
+  imageUri: beer.labels.large,
   price: randomInterval(2, 8).toFixed(2),
 }));
 
-const categories = beers.map((beer) => beer.style.category);
+const categories = beers.map((beer) => ({
+  id: beer.style.categoryId,
+  name: beer.style.category.name,
+}));
 const sortedUniqCategories = sortById(uniqById(categories));
 
 module.exports = () => ({
